84aded8127385f77b08c22112e439c40a54f857a
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-14 16:42:15 +0600

    global error handler

---
+++ src/infrastructure/middlewares/globalErrorHandler.ts
@@ -0,0 +1,49 @@
+import { ErrorRequestHandler } from 'express';
+import config from '../config/env.config';
+import { IErrorMessage } from '../../shared/types/errors.types';
+import ApiError from '../error/apiError';
+import handleZodError from '../error/zodError';
+
+const globalErrorHandler: ErrorRequestHandler = (error, req, res,) => {
+  let statusCode = 500;
+  let message = 'Something went wrong';
+  let errorMessages: IErrorMessage[] = [];
+
+  if (error.name === 'ZodError') {
+    const simplifiedError = handleZodError(error);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ApiError) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: '',
+            message: error.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : [];
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.node_env !== 'production' ? error?.stack : undefined,
+  });
+};
+
+export default globalErrorHandler;
