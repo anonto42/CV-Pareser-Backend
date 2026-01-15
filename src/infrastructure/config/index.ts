158166109eee536153f23eeed3b87d59ab6efe41
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-15 10:42:57 +0600

    removed

--- src/infrastructure/config/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-  port: Number(process.env.PORT),
-  node_env: process.env.NODE_ENV,
-  host: process.env.HOST?.toString() || 'localhost',
-};
