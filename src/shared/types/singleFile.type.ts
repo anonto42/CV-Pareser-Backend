173798b5a3ae5c427aee031a72fdf9b45b55862d
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-15 16:07:33 +0600

    single file type added

---
+++ src/shared/types/singleFile.type.ts
@@ -0,0 +1,12 @@
+
+export type SingleFile = {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  size: number;
+  destination: string;
+  filename: string;
+  path: string;
+  buffer?: Buffer;
+};
