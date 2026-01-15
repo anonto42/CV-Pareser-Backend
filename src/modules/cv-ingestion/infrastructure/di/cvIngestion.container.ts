9c4b72e9dbfd10253aa6466655f48940685bc109
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-15 20:11:27 +0600

    depandancy injaction container

--- src/modules/cv-ingestion/infrastructure/di/cvIngestion.container.ts
+++ src/modules/cv-ingestion/infrastructure/di/cvIngestion.container.ts
@@ -4,21 +4,26 @@ import { CVRepositoryAdapter } from '../adapters/output/persistence/CVRepository
 import { FileParserAdapter } from '../adapters/output/parsers/FileParser.adapter';
 import { CvIngestionController } from '../adapters/input/http/controllers/CvIngestion.controller';
 import { CvIngestionRoutes } from '../adapters/input/http/routes/cvIngestion.routes';
+import { DataForProcessUseCaseImpl } from '../../../cv-process/application/usecases/DataForProcess.usecase';
 
 export class CvIngestionContainer {
   static createRouter(): Router {
     
-    // Create output adapters (dependencies)
+    // Create output adapters
     const cvRepository = new CVRepositoryAdapter();
     const fileParser = new FileParserAdapter();
 
-    // Create use cases (application layer)
+    // External dependencies
+    const DataForProcessUseCase = new DataForProcessUseCaseImpl(); 
+
+    // Create use cases
     const uploadPdfUseCase = new UploadPDFCVUseCaseImpl(
       cvRepository,
-      fileParser
+      fileParser,
+      DataForProcessUseCase
     );
 
-    // Create controller (input adapter)
+    // Create controller
     const controller = new CvIngestionController(uploadPdfUseCase);
 
     // Create and return routes
