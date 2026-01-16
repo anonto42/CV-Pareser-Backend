6dab9b5f094af7c13d1aaa519e8142f80d24d2b5
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-17 01:14:45 +0600

    interface converted on type

--- src/modules/cv-chat/application/dto/ChatReturn.dto.ts
+++ src/modules/cv-chat/application/dto/ChatReturn.dto.ts
@@ -1,10 +1,10 @@
 import { StructuredCV } from "../../../../shared/application/dto/StructuredCV.dto";
 import { VectorRecord } from "../../../../shared/application/dto/VectorRecord.dto";
 
-export interface IChatReturn { 
-    answer: string; 
-    sources: { 
-      vectorRecords: VectorRecord[]; 
-      cvRecords: StructuredCV[] 
-    } 
-  }
+export type IChatReturn =  { 
+  answer: string; 
+  sources: { 
+    vectorRecords: VectorRecord[]; 
+    cvRecords: StructuredCV[] 
+  } 
+}
