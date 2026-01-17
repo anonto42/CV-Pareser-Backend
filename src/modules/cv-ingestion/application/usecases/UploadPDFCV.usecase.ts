e5293551f17315b518cf79cee2e7444875685236
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-17 10:23:00 +0600

    only data sended for the next execution

--- src/modules/cv-ingestion/application/usecases/UploadPDFCV.usecase.ts
+++ src/modules/cv-ingestion/application/usecases/UploadPDFCV.usecase.ts
@@ -4,27 +4,23 @@ import { SingleFile } from "../../../../shared/types/singleFile.type";
 import { ProcessDataFormet } from "../../../cv-process/application/dto/ProcessingDataFormet.dto";
 import { DataForProcessUseCase } from "../../../cv-process/application/ports/input/DataForProcessUseCase";
 import { UploadPDFCVUseCase } from "../ports/input/UploadPDFCVUseCase";
-import { CVRepositoryPort } from "../ports/output/CVRepository.port";
 import { FileParserPort } from "../ports/output/FileParser.port";
 
 export class UploadPDFCVUseCaseImpl implements UploadPDFCVUseCase {
   constructor(
-    private readonly cvRepository: CVRepositoryPort,
     private readonly fileParser: FileParserPort,
     private readonly dataForProcessUseCase: DataForProcessUseCase
   ) {}
 
-  async execute(file: SingleFile): Promise<string> {
+  async execute(file: SingleFile): Promise<void> {
     const parsedData = await this.fileParser.parsePDF(file);
 
     if (!parsedData) {
       throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, 'Failed to parse CV');
     }
+      
+    await this.dataForProcessUseCase.execute({text: parsedData.text, filePath: file.path});
 
-    const savedCV = await this.cvRepository.save(parsedData);
-      
-    await this.dataForProcessUseCase.execute(new ProcessDataFormet(parsedData.text, savedCV));
-      
-    return savedCV;
+    return;
   }
 }
