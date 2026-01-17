c2d45af4eda36b71bd37e05e18244742d5b9040d
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-17 10:22:27 +0600

    return type changed

--- src/modules/cv-ingestion/application/ports/input/UploadPDFCVUseCase.ts
+++ src/modules/cv-ingestion/application/ports/input/UploadPDFCVUseCase.ts
@@ -1,5 +1,5 @@
 import { SingleFile } from "../../../../../shared/types/singleFile.type";
 
 export interface UploadPDFCVUseCase {
-  execute(file: SingleFile): Promise<string>;
+  execute(file: SingleFile): Promise<void>;
 }
